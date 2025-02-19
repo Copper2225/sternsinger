15b60053e6cc5120eba04b6feb86bd8f74f1c36a
Niklas Verenkotte <niklas.verenkotte@check24.de>
2025-02-19 13:42:53 +0100

    Add Bauernschaften and use District Structure

--- frontend/src/View/Admin/Admin.tsx
+++ frontend/src/View/Admin/Admin.tsx
@@ -1,24 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, {useCallback, useEffect } from 'react';
 import { useRecoilState } from 'recoil';
-import {dorfBezirkeState} from "@/adminStore";
+import {bauernschaftenState, District, districtsState, dorfBezirkeState} from "@/adminStore";
 import DistrictLine from "@/View/Admin/DistrictLine";
 
 const Admin: React.FC = () => {
-    const [dorfBezirke] = useRecoilState(dorfBezirkeState); // Number of districts
-    const [values, setValues] = useState<number[]>([]); // District values
+    const [dorfBezirke] = useRecoilState(dorfBezirkeState);
+    const [bauernschaften] = useRecoilState(bauernschaftenState);
+    const [districts, setDistricts] = useRecoilState(districtsState);
     const backendURL = import.meta.env.VITE_BACKEND_URL;
 
     useEffect(() => {
-        // Fetch initial district values
         fetch(`${backendURL}/districts`)
             .then((response) => response.json())
-            .then((data) => setValues(data || []))
+            .then((data) => {
+                setDistricts((prevDistricts) => {
+                    return prevDistricts.map((district, index) => ({
+                        ...district,
+                        money: data[index]
+                    }));
+                });
+            })
             .catch((error) => console.error('Error fetching districts:', error));
-    }, []);
+    }, [backendURL]);
 
-    console.log(values);
+    useEffect(() => {
+        const newDistricts = Array.from({ length: dorfBezirke }, (_, index) => ({
+            name: `Bezirk ${index + 1}`,
+            counting: true,
+        }));
 
-    const handleSubmit = (value: number, index: number) => {
+        const newDistricts2 = bauernschaften.flatMap((b) => {
+            return Array.from({ length: b.amount }, (_, index) => ({
+                name: `${b.name}${b.amount > 1 ? ` ${index + 1}` : ""}`,
+                counting: true,
+            }));
+        });
+
+        setDistricts([...newDistricts, ...newDistricts2]);
+    }, [dorfBezirke, bauernschaften]);
+
+    const handleSubmit = useCallback((value: number, index: number) => {
         fetch(`${backendURL}/districts`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -27,20 +48,22 @@ const Admin: React.FC = () => {
             .then((response) => response.json())
             .then((data) => console.log(`Updated district ${index + 1}`, data))
             .catch((error) => console.error('Error updating district:', error));
-        setValues((prevValues) => {
+
+        setDistricts((prevValues) => {
             const newValues = [...prevValues];
-            newValues[index] = value;
+            newValues[index] = { ...newValues[index], money: value };
             return newValues;
         });
-    };
+    }, []);
+
 
     return (
         <div>
             <h1>Admin Page</h1>
             <table style={{maxWidth: "40%", marginTop: "20px"}}>
                 <tbody>
-                {Array.from({length: dorfBezirke}, (_, index) => (
-                    <DistrictLine key={index} name={"Bezirk " + (index + 1)} value={values[index] || ''}
+                {districts.map((district, index) => (
+                    <DistrictLine key={index} name={district.name} value={district.money ?? ''}
                                   handleSubmit={handleSubmit} index={index}/>
                 ))}
                 </tbody>
--- frontend/src/adminStore.ts
+++ frontend/src/adminStore.ts
@@ -1,6 +1,30 @@
 import {atom} from "recoil";
 
+export interface District {
+    name: string,
+    money?: number;
+    counting: boolean;
+}
+
+interface Bauernschaft {
+    name: string,
+    amount: number
+}
+
 export const dorfBezirkeState = atom<number>({
     key: 'dorfBezirke',
     default: 20,
+});
+
+export const bauernschaftenState = atom<Bauernschaft[]>({
+    key: 'bauernschaften',
+    default: [
+        { name: 'Brock', amount: 3 },
+        { name: 'Überwasser', amount: 1 }
+    ],
+})
+
+export const districtsState = atom<District[]>({
+    key: 'districts',
+    default: []
 });
