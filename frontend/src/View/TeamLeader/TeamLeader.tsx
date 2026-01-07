bbf628243d21d006c3ab6ce78f127bd0d67bd41f
Niklas Verenkotte <niklas.verenkotte@check24.de>
2026-01-07 15:13:32 +0100

    No user Login

--- frontend/src/View/TeamLeader/TeamLeader.tsx
+++ frontend/src/View/TeamLeader/TeamLeader.tsx
@@ -58,7 +58,7 @@ const TeamLeader = () => {
             });
 
             if (!res.ok) {
-                setNeedsDistrictAuth(true);
+                setNeedsDistrictAuth(false);
             }
         },
         [backendURL],
